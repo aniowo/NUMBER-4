0c927d0b620d875600d7d3cbc3afb51657317a50
aniowo <73297528+aniowo@users.noreply.github.com>
2020-11-10 08:48:53 +0100

    Add files via upload

---
+++ NUMBER 4/NUMBER 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUMBER_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                char[] Vowels = new char[5]
+                {
+                'a', 'e', 'i', 'o', 'u'
+                };
+                Console.Write("Enter an alphabet: ");
+                char alphabet = Convert.ToChar(Console.ReadLine());
+
+                int i = 0;
+                if (alphabet == Vowels[i])
+                {
+                    Console.WriteLine("The alphabet is a vowel");
+                }
+                else if (alphabet != Vowels[i])
+                {
+                    Console.WriteLine("The alphabet is a consonant");
+                }
+                else
+                {
+                    Console.WriteLine("ERROR IN INPUT");
+                }
+       
+            }
+            catch
+            {
+                Console.WriteLine("Please check your input");
+            }
+            Console.ReadLine();
+        }
+    }
+}
